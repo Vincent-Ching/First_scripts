fb649932b360aa389802e7a0adad09b041f7f645
Vincent cng <chingvincentdu20@yahoo.fr>
2024-10-22 15:50:39 +0200

    machine thé ou café

---
+++ index.js
@@ -0,0 +1,27 @@
+let order = "";
+let isConfirmed = false;
+
+while (isConfirmed === false) {
+    order = prompt("thé ou café?");
+    order = order + " " + prompt("avec ou sans sucre?");
+    order = order + " " + prompt("avec ou sans lait?");
+    
+    console.log(order);
+
+
+isConfirmed = true;    
+}
+
+while (isConfirmed) {
+
+    let drink = "";
+
+    let sugar = "";
+    
+    let milk = "";
+
+    let milkType = "";
+
+    order = "commande totale"
+}
+console.log("La Commande est en route");
